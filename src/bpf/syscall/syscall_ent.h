9bc7973bdd0200d4ccaa0c2f8dc05ef439d78f79
RinHizakura <s921975628@gmail.com>
2023-03-30 17:57:07 +0800

    Check the arguments structure size

--- src/bpf/syscall/syscall_ent.h
+++ src/bpf/syscall/syscall_ent.h
@@ -1,13 +1,23 @@
 #ifndef SYSCALL_ENT_H
 #define SYSCALL_ENT_H
 
+/* Reference:
+ * - https://elixir.bootlin.com/linux/latest/source/include/linux/build_bug.h */
+#define static_assert(expr, ...) __static_assert(expr, ##__VA_ARGS__, #expr)
+#define __static_assert(expr, msg, ...) _Static_assert(expr, msg)
+
 #define BUF_SIZE 32
+
+typedef struct {
+    u8 args[64];
+} args_t;
 
 typedef struct {
     int fd;
     u8 buf[BUF_SIZE];
     size_t count;
 } read_args_t;
+static_assert(sizeof(read_args_t) <= sizeof(args_t));
 
 /* FIXME: This structure is designed to be used in
  * both C and Rust. However, this is not pretty :( */
@@ -16,9 +26,10 @@ typedef struct {
     u64 ret;
 
     union {
-        /* FIXME: We should make sure the size of every args
-         * structure not extend 64 bytes */
-        u8 args[64];
+        /* Note: We should make sure that args_t is the
+         * largest struct under the union. This guarantee
+         * the structure layout between C and Rust code. */
+        args_t args;
         read_args_t read;
     };
 } syscall_ent_t;
