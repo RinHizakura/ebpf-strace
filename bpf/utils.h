728e13614b091495191c5a2111004d82b51b1fa0
RinHizakura <s921975628@gmail.com>
2023-04-19 22:55:49 +0800

    Fix strlen implementation

--- bpf/utils.h
+++ bpf/utils.h
@@ -43,9 +43,9 @@ static size_t strlen(const char *str)
 {
     size_t len;
 
-    for (len = 0; str[len]; len++)
+    for (len = 0; str[len]; len++) {}
 
-        return len;
+    return len;
 }
 
 #endif
