a9b8febab5f37d86bdc4d73564ad16bb9b3e60e8
RinHizakura <s921975628@gmail.com>
2023-05-09 01:52:11 +0800

    Add test for openat

--- tests/do_syscall.c
+++ tests/do_syscall.c
@@ -3,14 +3,40 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-void do_file_operation()
+#define TEST(f)       \
+    if(f())           \
+        return -1     \
+
+int do_file_operation()
 {
     long fd = syscall(SYS_open, "README.md", O_RDONLY);
+    if (fd < 0)
+        return -1;
     syscall(SYS_close, fd);
+
+    return 0;
+}
+
+int do_file_operation_2()
+{
+    int dirfd = open("../ebpf-strace", O_DIRECTORY | O_RDONLY);
+    if (dirfd < 0)
+        return -1;
+
+    int fd = openat(dirfd, "README.md", O_RDONLY);
+    if (fd < 0)
+        return -1;
+
+    close(fd);
+    close(dirfd);
+
+    return 0;
 }
 
 int main()
 {
-    do_file_operation();
+    //TEST(do_file_operation);
+    TEST(do_file_operation_2);
+
     return 0;
 }
