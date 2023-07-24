a81be58409c9d1f1f6263e0d4ccf7811492b69b2
RinHizakura <s921975628@gmail.com>
2023-07-24 22:52:18 +0800

    Fix signal test to avoid non-Async-Signal-Safe API

--- tests/signal.c
+++ tests/signal.c
@@ -1,19 +1,52 @@
 #include <signal.h>
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
+
+static bool getsig = false;
+static struct {
+    int sig;
+    siginfo_t info;
+} sig_message;
 
 static void handler(int sig,
                     siginfo_t *info,
                     __attribute__((unused)) void *ucontext)
 {
-    /* FIXME: The printf function is not Async-Signal-Safety,
-     * considering other replacement. */
-    if (sig == SIGUSR1 && info->si_code == SI_TKILL)
-        printf(
-            "--- SIGUSR1 {si_signo=SIGUSR1, si_code=SI_TKILL, si_pid=%d, "
-            "si_uid=%d} ---\n",
-            info->si_pid, info->si_uid);
+    if (getsig == false) {
+        sig_message.sig = sig;
+        memcpy(&(sig_message.info), info, sizeof(siginfo_t));
+    }
+
+    getsig = true;
+}
+
+static char *signame(int sig)
+{
+    switch (sig) {
+    case SIGUSR1:
+        return "SIGUSR1";
+    default:
+        return "Unknown";
+    }
+}
+
+static void format_signal(int sig, siginfo_t *info)
+{
+    printf("--- %s {si_signo=%s", signame(sig), signame(info->si_signo));
+
+    switch (info->si_code) {
+    case SI_TKILL:
+        printf(", si_code=SI_TKILL, si_pid=%d, si_uid=%d", info->si_pid,
+               info->si_uid);
+        break;
+    default:
+        break;
+    }
+
+    printf("} ---\n");
 }
 
 #define RT_0 32
@@ -24,6 +57,7 @@ int main()
     sigaddset(&set, SIGUSR2);
     sigaddset(&set, SIGCHLD);
     sigaddset(&set, RT_0 + 3);
+
     int result = sigprocmask(SIG_SETMASK, &set, &oldset);
     if (result < 0)
         return -1;
@@ -39,6 +73,9 @@ int main()
 
     if (raise(SIGUSR1))
         return -1;
+    while (!getsig)
+        ;  // busy waiting until we get the first signal
+    format_signal(sig_message.sig, &sig_message.info);
 
     result = sigprocmask(SIG_SETMASK, &oldset, NULL);
     if (result < 0)
